80498edb3667b36d198dd3dc4b6db9a90a5c8fe3
Celorio2001 <jahir.celorio2001@gmail.com>
2023-04-06 11:48:06 -0500

    add model and update view

--- database/migrations/2023_04_06_153839_create_products.php
+++ database/migrations/2023_04_06_153839_create_products.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('ProductName');
-            $table->integer('SupplierID');
-            $table->integer('CatedoryID');
+            $table->string('SupplierID');
+            $table->string('CatedoryID');
             $table->integer('Unit');
             $table->decimal('Price');
         });
--- resources/views/Pages/view_ventas.blade.php
+++ resources/views/Pages/view_ventas.blade.php
@@ -1,1 +1,24 @@
-<h1>Hola Mundo</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+        <form action="" class="form" style="width: 800px; position: relative;left: 10px;">
+            <label for="product_name" class="form-text">Nombre de Producto</label>
+            <input id="product_name" type="text" class="form-control mt-2  p-1">
+            <label for="supplier_id" class="form-text">Proveedor</label>
+            <input id="supplier_id" type="text" class="form-control mt-2  p-1">
+            <label for="category_id" class="form-text">Categoría</label>
+            <input id="category_id" type="text" class="form-control mt-2  p-1">
+            <label for= "unit" class="form-text">Unidad</label>
+            <input id="unit" type="number" class="form-control mt-2  p-1">
+            <label for="price" class="form-text">Precio</label>
+            <input id="price" type="number" class="form-control mt-2  p-1">
+            <button class="btn btn-danger mt-3">Enviar</button>
+        </form>
+</body>
+</html>
